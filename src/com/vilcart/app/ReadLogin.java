f4c91185fd55b976f512d54b87a8a96fdaa329f0
suhailsqm <suhailsqm@gmail.com>
2022-07-28 15:12:44 +0530

    only heroes testng code.

--- src/com/vilcart/app/ReadLogin.java
+++ src/com/vilcart/app/ReadLogin.java
@@ -45,6 +45,8 @@ import com.vilcart.pom.*;
 import org.openqa.selenium.interactions.Actions;
 import com.paulhammant.ngwebdriver.NgWebDriver;
 import com.paulhammant.ngwebdriver.ByAngular;
+import com.paulhammant.ngwebdriver.ByAngularModel;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ReadLogin {
@@ -53,33 +55,23 @@ public class ReadLogin {
 
     WebDriver driver;
     NgWebDriver ngWebDriver;
-    WebDriverWait wait;
-    XSSFWorkbook workbook;
-    XSSFSheet sheet;
-    XSSFCell cell;
+    //WebDriverWait wait;
+    //XSSFWorkbook workbook;
+    //XSSFSheet sheet;
+    //XSSFCell cell;
 
     
     @BeforeTest
     public void TestSetup()
 	{
-    	//WebDriverManager.chromedriver().setup();
-    	//driver = new ChromeDriver();
-    	 WebDriverManager.firefoxdriver().setup();
-    	 driver = new FirefoxDriver();
 
-    	//ChromeOptions options = new ChromeOptions();
-    	//options.setBinary("C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe");
-    	//System.setProperty("webdriver.chrome.driver","C:\\Users\\win10\\Downloads\\chromedriver_win32_104\\chromedriver.exe");
-    	//driver = new ChromeDriver(options);
+    	WebDriverManager.firefoxdriver().setup();
+    	driver = new FirefoxDriver();
     	ngWebDriver = new NgWebDriver((JavascriptExecutor) driver);
-       // Enter url.
-       driver.get("https://vilcart-buy.web.app/");
-       driver.manage().window().maximize();
-        
-       //wait = new WebDriverWait(driver,30);
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-       assertThat(driver.getTitle()).containsIgnoringCase("Login - VILCART");
-       Reporter.log(driver.getTitle(), true);
+    	driver.get("https://angular-tour-of-heroes-1a2a8.web.app/");
+    	driver.manage().window().maximize(); 
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    	Reporter.log(driver.getTitle(), true);
      }
     @AfterTest
     public void TestTeardown() throws InterruptedException 
@@ -89,70 +81,12 @@ public class ReadLogin {
     }
     @Test
     public void ReadData() throws FileNotFoundException, IOException, NoSuchElementException, InterruptedException, JavascriptException {
-    	// Import excel sheet.
-        //File src=new File("C:\\Users\\win10\\eclipse-workspace\\FirstTestNG\\TestData.xlsx");
-    	File src=new File("TestData.xlsx");
-        // Load the file.
-        FileInputStream finput = new FileInputStream(src);
-         
-        // Load he workbook.
-         workbook = new XSSFWorkbook(finput);
-         DataFormatter formatter = new DataFormatter();
-        // Load the sheet in which data is stored.
-        sheet= workbook.getSheetAt(0);
-        for(int i=1; i<=sheet.getLastRowNum(); i++)
-        {
-            // Import data for Email.
-            cell = sheet.getRow(i).getCell(2);
-            //cell.setCellType(Cell.CELL_TYPE_STRING);
-            String value = formatter.formatCellValue(cell);
-            driver.findElement(By.cssSelector("input[ng-reflect-name=email]")).sendKeys(value);
-             
-            // Import data for password.
-            cell = sheet.getRow(i).getCell(3);
-            //cell.setCellType(Cell.CELL_TYPE_STRING);
-            value = formatter.formatCellValue(cell);
-            driver.findElement(By.cssSelector("input[ng-reflect-name=password]")).sendKeys(value);
-            
-            driver.findElement(By.tagName("button")).click();
-            
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-            
-            Wait<WebDriver> wait= new FluentWait<WebDriver>(driver)
-                    .withTimeout(Duration.ofSeconds(30))
-                    .pollingEvery(Duration.ofSeconds(5))
-                    .ignoring(NoSuchElementException.class);
-            
-            //log.info("Home page title: \n"+ driver.getTitle());
-            //log.debug("Home page title: \n"+ driver.getTitle());
-            Thread.sleep(3000);
-            ngWebDriver.waitForAngularRequestsToFinish();
-            Reporter.log(driver.getTitle(), true);
-            assertThat(driver.getTitle()).containsIgnoringCase("Home - VILCART");
-            
-            driver.findElement(By.xpath("//*[@id=\"main-menu-navigation\"]/li[2]/a/span")).click();
-            
-            driver.findElement(By.xpath("//*[@id=\"main-menu-navigation\"]/li[2]/ul/li[1]/a")).click();
-            
-            //WebElement state = driver.findElement(By.xpath("/html/body/app-main/app-private-layout/app-create-customers/div/div/div[2]/section/div/div/m-card/div/div[2]/div/div/div[2]/div[1]/div/ng-select/div/div/div[3]/input"));
-            Actions action = new Actions(driver);
-            //state.click();
-            //action.moveToElement(state).click().build().perform();
-            //ngWebDriver.waitForAngularRequestsToFinish();
-            
-            Reporter.log("Before model state",true);
-            //Thread.sleep(10000);
-            
-            NgWebDriver ngwd = new NgWebDriver((JavascriptExecutor)driver).withRootSelector("\"app-create-customers\"");
-            ByAngular.Factory baf = ngwd.makeByAngularFactory();
-            WebElement stateSelect = driver.findElement(baf.model("state"));
-            
-            //WebElement stateSelect = driver.findElement(ByAngular.withRootSelector("\"app-create-customers\"").model("state"));
-            stateSelect.click();
-
-
-            Thread.sleep(2000);
-        }
-        finput.close();
+    	
+    	ngWebDriver.waitForAngularRequestsToFinish();
+        //WebElement stateSelect = driver.findElement(ByAngular.withRootSelector("\"app-root\"").model("hero.name"));
+        WebElement stateSelect = driver.findElement(ByAngular.withRootSelector("\"app-heroes\"").model("hero.name"));
+        //WebElement stateSelect = driver.findElement(ByAngularModel.name("hero.name"));
+        stateSelect.sendKeys("Testing Angular");
+    	
     }
 }
